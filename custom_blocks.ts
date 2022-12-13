f396b506c82fd91373c5ff4aafd18aa3efebb9a8
MaximVansteenkiste <47556310+MaximVansteenkiste@users.noreply.github.com>
2022-12-13 12:57:12 +0100

    custom_blocks.ts bijwerken

--- custom_blocks.ts
+++ custom_blocks.ts
@@ -4,10 +4,11 @@ namespace CodeCosmos {
     //% block.loc.nl="ontgrendelWereld $code"
     export function unlockWorld(code: string) {
         if (code === "code123") {
-            const duration = 20*60*60
+            const duration = 20*60*90
 
             player.execute(`scoreboard players set @a lock_clock ${duration}`)
-            player.execute("effect @s clear")
+            player.execute("effect @s clear");
+            player.execute("fill ~-2 ~ ~-2 ~2 ~2 ~2 air")
         } else {
             player.execute("say Wrong code!");
         }
