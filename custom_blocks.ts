ad2c1cfb6c0eb5df6db3ce39748684d45ff545cf
Bram Verhulst <brammieverhulst@gmail.com>
2024-09-12 13:20:49 +0200

    Hey tor

--- custom_blocks.ts
+++ custom_blocks.ts
@@ -1,12 +1,26 @@
 //% color=190 weight=100 block="CodeCosmos"
 namespace CodeCosmos {
-    //% block="ontgrendelWereld $code"
+
+    let codes = {
+        reeks2missie8: "code20232024"
+    }
+
+
+    //% block="unlockWorld $code"
     export function unlockWorld(code: string) {
         if (code === "code20232024") {
-            const duration = 20*60*60*3
+            const duration = 20 * 60 * 60 * 3
 
             player.execute(`scoreboard players set @a lock_clock ${duration}`)
             player.execute("effect @s clear")
         }
     }
+
+    //% block="unlockWorld $code"
+    export function unlockWorldReeks2Missie8(code: string) {
+        if (code === codes.reeks2missie8) {
+            player.execute("scriptevent cc:auth");
+        }
+    }
+    
 }
