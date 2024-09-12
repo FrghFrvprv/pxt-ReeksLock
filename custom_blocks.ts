e4765436f0980bf2b89328843db777211ea9ccc7
DyslexicWolf <125255208+DyslexicWolf@users.noreply.github.com>
2024-09-12 13:57:12 +0200

    all codes and md files included

--- custom_blocks.ts
+++ custom_blocks.ts
@@ -2,13 +2,40 @@
 namespace CodeCosmos {
 
     let codes = {
-        reeks2missie8: "code20232024"
+        reeks1missie1: "code1",
+        reeks1missie2: "code2",
+        reeks1missie3: "code3",
+        reeks1missie4: "code4",
+        reeks1missie5: "code5",
+        reeks1missie6: "code6",
+        reeks1missie7: "code7",
+        reeks1missie8: "code8",
+
+        reeks2missie1: "code9",
+        reeks2missie2: "code10",
+        reeks2missie3: "code11",
+        reeks2missie4: "code12",
+        reeks2missie5: "code13",
+        reeks2missie6: "code14",
+        reeks2missie7: "code15",
+        reeks2missie8: "code16",
+        reeks2missie9: "code17"
+
     }
 
+    //% block="unlockWorld reeks1missie1 $code"
+    export function unlockWorldReeks1Missie1(code: string) {
+        if (code === codes.reeks1missie1) {
+            const duration = 20 * 60 * 60 * 3
 
-    //% block="unlockWorld $code"
-    export function unlockWorld(code: string) {
-        if (code === "code20232024") {
+            player.execute(`scoreboard players set @a lock_clock ${duration}`)
+            player.execute("effect @s clear")
+        }
+    }
+    
+    //% block="unlockWorld reeks1missie2 $code"
+    export function unlockWorldReeks1Missie2(code: string) {
+        if (code === codes.reeks1missie2) {
             const duration = 20 * 60 * 60 * 3
 
             player.execute(`scoreboard players set @a lock_clock ${duration}`)
@@ -16,11 +43,135 @@ namespace CodeCosmos {
         }
     }
 
-    //% block="unlockWorld $code"
+    //% block="unlockWorld reeks1missie3 $code"
+    export function unlockWorldReeks1Missie3(code: string) {
+        if (code === codes.reeks1missie3) {
+            const duration = 20 * 60 * 60 * 3
+
+            player.execute(`scoreboard players set @a lock_clock ${duration}`)
+            player.execute("effect @s clear")
+        }
+    }
+
+    //% block="unlockWorld reeks1missie4 $code"
+    export function unlockWorldReeks1Missie4(code: string) {
+        if (code === codes.reeks1missie4) {
+            const duration = 20 * 60 * 60 * 3
+
+            player.execute(`scoreboard players set @a lock_clock ${duration}`)
+            player.execute("effect @s clear")
+        }
+    }
+
+    //% block="unlockWorld reeks1missie5 $code"
+    export function unlockWorldReeks1Missie5(code: string) {
+        if (code === codes.reeks1missie5) {
+            const duration = 20 * 60 * 60 * 3
+
+            player.execute(`scoreboard players set @a lock_clock ${duration}`)
+            player.execute("effect @s clear")
+        }
+    }
+
+    //% block="unlockWorld reeks1missie6 $code"
+    export function unlockWorldReeks1Missie6(code: string) {
+        if (code === codes.reeks1missie6) {
+            const duration = 20 * 60 * 60 * 3
+
+            player.execute(`scoreboard players set @a lock_clock ${duration}`)
+            player.execute("effect @s clear")
+        }
+    }
+
+    //% block="unlockWorld reeks1missie7 $code"
+    export function unlockWorldReeks1Missie7(code: string) {
+        if (code === codes.reeks1missie7) {
+            const duration = 20 * 60 * 60 * 3
+
+            player.execute(`scoreboard players set @a lock_clock ${duration}`)
+            player.execute("effect @s clear")
+        }
+    }
+
+    //% block="unlockWorld reeks1missie8 $code"
+    export function unlockWorldReeks1Missie8(code: string) {
+        if (code === codes.reeks1missie8) {
+            const duration = 20 * 60 * 60 * 3
+
+            player.execute(`scoreboard players set @a lock_clock ${duration}`)
+            player.execute("effect @s clear")
+        }
+    }
+
+
+
+    //% block="unlockWorld reeks2missi1 $code"
+    export function unlockWorldReeks2Missie1(code: string) {
+        if (code === codes.reeks2missie1) {
+            player.execute("scriptevent cc:auth");
+        }
+    }
+        
+    //% block="unlockWorld reeks2missi2 $code"
+    export function unlockWorldReeks2Missie2(code: string) {
+        if (code === codes.reeks2missie2) {
+            player.execute("scriptevent cc:auth");
+        }
+    }
+    
+    //% block="unlockWorld reeks2missi3 $code"
+    export function unlockWorldReeks2Missie3(code: string) {
+        if (code === codes.reeks2missie3) {
+            player.execute("scriptevent cc:auth");
+        }
+    }
+    
+    //% block="unlockWorld reeks2missi3 $code"
+    export function unlockWorldReeks2Missie3(code: string) {
+        if (code === codes.reeks2missie3) {
+            player.execute("scriptevent cc:auth");
+        }
+    }
+    
+    //% block="unlockWorld reeks2missi4 $code"
+    export function unlockWorldReeks2Missie4(code: string) {
+        if (code === codes.reeks2missie4) {
+            player.execute("scriptevent cc:auth");
+        }
+    }
+
+    //% block="unlockWorld reeks2missi5 $code"
+    export function unlockWorldReeks2Missie5(code: string) {
+        if (code === codes.reeks2missie5) {
+            player.execute("scriptevent cc:auth");
+        }
+    }
+
+    //% block="unlockWorld reeks2missi6 $code"
+    export function unlockWorldReeks2Missie6(code: string) {
+        if (code === codes.reeks2missie6) {
+            player.execute("scriptevent cc:auth");
+        }
+    }
+
+    //% block="unlockWorld reeks2missi7 $code"
+    export function unlockWorldReeks2Missie7(code: string) {
+        if (code === codes.reeks2missie7) {
+            player.execute("scriptevent cc:auth");
+        }
+    }
+
+    //% block="unlockWorld reeks2missi8 $code"
     export function unlockWorldReeks2Missie8(code: string) {
         if (code === codes.reeks2missie8) {
             player.execute("scriptevent cc:auth");
         }
     }
-    
+
+    //% block="unlockWorld reeks2missi9 $code"
+    export function unlockWorldReeks2Missie9(code: string) {
+        if (code === codes.reeks2missie9) {
+            player.execute("scriptevent cc:auth");
+        }
+    }
 }
---
+++ reeks2/t_reeks2missie6.md
@@ -0,0 +1,13 @@
+### @hideIteration true
+### @flyoutOnly true
+# Unlock (v1.2)
+```blocks
+CodeCosmos.unlockWorldReeks2Missie6("code")
+```
+
+```template
+CodeCosmos.unlockWorldReeks2Missie6("code")
+```
+
+## Unlock world
+Fill in the code from the learningplatform and press the big, green play button.
